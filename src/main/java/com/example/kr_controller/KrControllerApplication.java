d08d333f665ed825fd5bc9909a516c5ac907e3cc
Dmitry Ostapov <OstapovD@terralink-global.com>
2023-08-21 14:04:31 +0300

    1

---
+++ src/main/java/com/example/kr_controller/KrControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kr_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kr_controller/KrControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kr_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
