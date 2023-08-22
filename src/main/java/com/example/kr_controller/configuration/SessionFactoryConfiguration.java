045e04e7bb4baf305defda6da87deaf0006c564a
VladislavCat <kshift84@gmail.com>
2023-08-22 18:03:15 +0300

    Devopes V0.0.3 Docker Version

--- src/main/java/com/example/kr_controller/configuration/SessionFactoryConfiguration.java
+++ src/main/java/com/example/kr_controller/configuration/SessionFactoryConfiguration.java
@@ -1,7 +1,6 @@
 package com.example.kr_controller.configuration;
 
 import com.example.kr_controller.model.Hello;
-import com.example.kr_controller.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +18,6 @@ public class SessionFactoryConfiguration {
     public static org.hibernate.cfg.Configuration buildConfiguration() {
         org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
         configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-        configuration.addAnnotatedClass(User.class);
         configuration.addAnnotatedClass(Hello.class);
         return configuration;
     }
--- src/main/java/com/example/kr_controller/service/HelloService.java
+++ src/main/java/com/example/kr_controller/service/HelloService.java
@@ -1,13 +1,10 @@
 package com.example.kr_controller.service;
 
 import com.example.kr_controller.model.Hello;
-import com.example.kr_controller.model.User;
 import com.example.kr_controller.repository.HelloRepository;
-import com.example.kr_controller.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
