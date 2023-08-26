92bb5f0cd48fe7a9ca8f71d2b0dc02c7005dcfaf
Dmitry Ostapov <OstapovD@terralink-global.com>
2023-08-26 17:25:17 +0300

    filter

--- src/main/java/com/example/kr_controller/dto/UserDto.java
+++ src/main/java/com/example/kr_controller/dto/UserDto.java
@@ -1,8 +1,10 @@
 package com.example.kr_controller.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class UserDto {
     String name;
     String surname;
