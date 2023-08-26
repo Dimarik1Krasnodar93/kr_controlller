0147874a4589f2d92f4cdef0bda82cba2be1c7ba
Dmitry Ostapov <OstapovD@terralink-global.com>
2023-08-26 18:05:39 +0300

    exchange

--- src/main/java/com/example/kr_controller/config/MyConfig.java
+++ src/main/java/com/example/kr_controller/config/MyConfig.java
@@ -1,8 +1,10 @@
 package com.example.kr_controller.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
@@ -11,6 +13,12 @@ import java.util.Properties;
 
 @Configuration
 public class MyConfig {
+    @Value("${source-api-uri}")
+    private String uri;
+    @Bean
+    public WebClient webClient() {
+        return WebClient.create(uri);
+    }
     @Bean
     public DataSource dataSource() {
         Properties properties = new Properties();
--- src/main/java/com/example/kr_controller/service/IndexService.java
+++ src/main/java/com/example/kr_controller/service/IndexService.java
@@ -1,7 +1,28 @@
 package com.example.kr_controller.service;
 
+import com.example.kr_controller.dto.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class IndexService {
+    @Value("${source-api-uri}")
+    private String uri;
+    private final WebClient client;
+
+    public List<UserDto> findAll() {
+        List list = client
+                .get()
+                .uri("%s/users/findAll".formatted(uri))
+                .retrieve()
+                .bodyToMono(List.class)
+                .block();
+        return list;
+    }
 }
