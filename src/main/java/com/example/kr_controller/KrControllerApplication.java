57ab940e7976c80affd9e83c2d8e627f6e6d95f5
VladislavCat <kshift84@gmail.com>
2023-09-04 14:42:08 +0300

    0.1.2 tempCommit

--- src/main/java/com/example/kr_controller/KrControllerApplication.java
+++ src/main/java/com/example/kr_controller/KrControllerApplication.java
@@ -1,14 +1,27 @@
 package com.example.kr_controller;
 
+import liquibase.integration.commandline.Main;
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 //@EnableConfigurationProperties(JwtProperties.class)
-public class KrControllerApplication {
+public class KrControllerApplication extends SpringBootServletInitializer {
+
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(KrControllerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KrControllerApplication.class, args);
 	}
-
 }
