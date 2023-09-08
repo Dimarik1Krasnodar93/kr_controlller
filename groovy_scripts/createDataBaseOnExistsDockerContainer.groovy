1acde1d33822d58aeae3cde315d99d2e37c47bfa
VladislavCat <kshift84@gmail.com>
2023-09-08 15:25:34 +0300

    0.2.4 Creating script for config db

---
+++ groovy_scripts/createDataBaseOnExistsDockerContainer.groovy
@@ -0,0 +1,52 @@
+import java.util.stream.Collectors
+
+String commandToJoinDb = "docker container exec -it db psql -U postgres -w -d  postgres"
+def process = Runtime.runtime.exec(commandToJoinDb)
+BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+if (!br.lines().findFirst().get().toLowerCase(Locale.ROOT).contains("error")) {
+    def strPath = (System.getProperty("user.dir"))
+    def envPath = strPath + "/.env"
+
+    BufferedReader reader
+            = new BufferedReader(new FileReader(new File(envPath)))
+
+    def map = reader.lines()
+            .map(str -> str.split("="))
+            .collect(Collectors.toMap(e
+                    -> e[0].split("_")[1]
+                    .toLowerCase(Locale.ROOT), e -> e[1]))
+    reader.close()
+
+    String command = "CREATE DATABASE " + map.get("db") + ";"
+    process = Runtime.runtime.exec(command)
+    br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+    if (!br.lines().findFirst().get().toLowerCase(Locale.ROOT).contains("error")) {
+        println("======СОЗДАНА БАЗА ДАННЫХ " + map.get("db") + "======")
+    } else {
+        println("======БАЗА ДАННЫХ " + map.get("db") + " УЖЕ СУЩЕСТВУЕТ.======")
+    }
+
+    command = "CREATE USER " + map.get("user") + " WITH PASSWORD \'" + map.get("password") + "\' SUPERUSER;"
+    process = Runtime.runtime.exec(command);
+    br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+    if (!br.lines().findFirst().get().toLowerCase(Locale.ROOT).contains("error")) {
+        println("======СОЗДАН ПОЛЬЗОВАТЕЛЬ " + map.get("user") + "======")
+    } else {
+        boolean start = true;
+        while (start) {
+            println("======ПОЛЬЗОВАТЕЛЬ " + map.get("user") + " УЖЕ СУЩЕСТВУЕТ.======")
+            Scanner scanner = new Scanner(System.in);
+            println("Изменить пароль? Y/N")
+            String s = scanner.nextLine();
+            if (s.toLowerCase(Locale.ROOT) == "y") {
+                command = "ALTER USER " + map.get("user") + "WITH PASSWORD \'" + map.get("password") + "\';"
+                Runtime.runtime.exec(command);
+            } else if (s.toLowerCase(Locale.ROOT) == "n") {
+                "======ОК======"
+                start = false;
+            }
+        }
+    }
+}
